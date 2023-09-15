daf7fff06c9d8940d3ecad08b7660b61595e6dc5
Risbob Goku <bhat.rish@gmail.com>
2023-09-15 01:30:55 -0700

    Started building .py file

---
+++ IrisClassification.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+
+
+iris_df = sns.load_dataset('iris')
+print(iris_df.head())
